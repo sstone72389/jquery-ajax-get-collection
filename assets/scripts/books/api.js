531940f71fe6aaa4b7da35d37b22cbab49d896b2
Jeffrey Horn <jeffh@generalassemb.ly>
2017-01-19 12:26:26 -0500

    Add solution
    Refresh for cohort 016

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -1,6 +1,14 @@
 'use strict';
 
+const app = require('../app.js');
+
+const index = function () {
+  return $.ajax({
+    url: app.host + '/books',
+    method: 'GET',
+  });
+};
 
 module.exports = {
-
+  index
 };
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,10 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const bookEvents = require('./books/events');
+
+// On document ready
+$(() => {
+  $('.books').on('submit', bookEvents.onGetBooks)
+});
